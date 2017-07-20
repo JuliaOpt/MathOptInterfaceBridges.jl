618fa9e7735a72fbe81d139a79aed048409b14c1
Benoît Legat <benoit.legat@gmail.com>
2017-07-20 12:26:26 +0200

    MathOptInterfaceUtilities.jl generated files.
        license:  MIT
        authors:  Benoît Legat
        years:    2017
        user:     
    
    Julia Version 0.6.0 [903644385b*]

---
+++ src/MathOptInterfaceUtilities.jl
@@ -0,0 +1,5 @@
+module MathOptInterfaceUtilities
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using MathOptInterfaceUtilities
+using Base.Test
+
+# write your own tests here
+@test 1 == 2
