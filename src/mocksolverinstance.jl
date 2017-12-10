a5b355309c7a1150643eee5599de84ff9eae9be6
Miles Lubin <mlubin@google.com>
2017-12-10 10:42:40 -0500

    implement a mock (fake) solver instance for testing

---
+++ src/mocksolverinstance.jl
@@ -0,0 +1,107 @@
+
+# An Int-valued attribute
+struct MockInstanceAttribute <: MOI.AbstractInstanceAttribute
+end
+
+# An Int-valued attribute
+struct MockVariableAttribute <: MOI.AbstractVariableAttribute
+end
+
+# An Int-valued attribute
+struct MockConstraintAttribute <: MOI.AbstractConstraintAttribute
+end
+
+# A mock solver instance used for testing.
+mutable struct MockSolverInstance <: MOI.AbstractSolverInstance
+    instance::MOI.AbstractStandaloneInstance
+    attribute::Int # MockInstanceAttribute
+    varattribute::Dict{MOI.VariableIndex,Int} # MockVariableAttribute
+    conattribute::Dict{MOI.ConstraintIndex,Int} # MockConstraintAttribute
+    solved::Bool
+    terminationstatus::MOI.TerminationStatusCode
+    resultcount::Int
+    objectivevalue::Float64
+    primalstatus::MOI.ResultStatusCode
+    varprimal::Dict{MOI.VariableIndex,Float64}
+    # TODO: constraint primal
+    # TODO: dual status and dual result
+end
+
+MockSolverInstance(instance::MOI.AbstractStandaloneInstance) =
+    MockSolverInstance(instance,
+                       0,
+                       Dict{MOI.VariableIndex,Int}(),
+                       Dict{MOI.ConstraintIndex,Int}(),
+                       false,
+                       MOI.Success,
+                       0,
+                       NaN,
+                       MOI.UnknownResultStatus,
+                       Dict{MOI.VariableIndex,Float64}())
+
+MOI.addvariable!(mock::MockSolverInstance) = MOI.addvariable!(mock.instance)
+MOI.addvariables!(mock::MockSolverInstance, n::Int) = MOI.addvariables!(mock.instance, n)
+MOI.addconstraint!(mock::MockSolverInstance, F, S) = MOI.addconstraint!(mock.instance, F, S)
+MOI.optimize!(mock::MockSolverInstance) = (mock.solved = true)
+
+MOI.canset(mock::MockSolverInstance, ::Union{MOI.ResultCount,MOI.TerminationStatus,MOI.ObjectiveValue,MOI.PrimalStatus,MockInstanceAttribute}) = true
+MOI.canset(mock::MockSolverInstance, ::Union{MOI.VariablePrimal,MockVariableAttribute}, ::MOI.VariableIndex) = true
+MOI.canset(mock::MockSolverInstance, ::Union{MOI.VariablePrimal,MockVariableAttribute}, ::Vector{MOI.VariableIndex}) = true
+MOI.canset(mock::MockSolverInstance, ::MockConstraintAttribute, ::MOI.ConstraintIndex) = true
+MOI.canset(mock::MockSolverInstance, ::MockConstraintAttribute, ::Vector{<:MOI.ConstraintIndex}) = true
+
+MOI.set!(mock::MockSolverInstance, ::MOI.ResultCount, value::Integer) = (mock.resultcount = value)
+MOI.set!(mock::MockSolverInstance, ::MOI.TerminationStatus, value::MOI.TerminationStatusCode) = (mock.terminationstatus = value)
+MOI.set!(mock::MockSolverInstance, ::MOI.ObjectiveValue, value::Real) = (mock.objectivevalue = value)
+MOI.set!(mock::MockSolverInstance, ::MOI.PrimalStatus, value::MOI.ResultStatusCode) = (mock.primalstatus = value)
+MOI.set!(mock::MockSolverInstance, ::MOI.DualStatus, value::MOI.ResultStatusCode) = (mock.dualstatus = value)
+MOI.set!(mock::MockSolverInstance, ::MockInstanceAttribute, value::Integer) = (mock.attribute = value)
+
+MOI.set!(mock::MockSolverInstance, ::MOI.VariablePrimal, idx::MOI.VariableIndex, value) = (mock.varprimal[idx] = value)
+MOI.set!(mock::MockSolverInstance, ::MockVariableAttribute, idx::MOI.VariableIndex, value) = (mock.varattribute[idx] = value)
+MOI.set!(mock::MockSolverInstance, ::MockConstraintAttribute, idx::MOI.ConstraintIndex, value) = (mock.conattribute[idx] = value)
+function MOI.set!(mock::MockSolverInstance, ::MOI.VariablePrimal, idx::Vector{MOI.VariableIndex}, value)
+    for (i,v) in zip(idx, value)
+        mock.varprimal[i] = v
+    end
+end
+function MOI.set!(mock::MockSolverInstance, ::MockVariableAttribute, idx::Vector{MOI.VariableIndex}, value)
+    for (i,v) in zip(idx, value)
+        mock.varattribute[i] = v
+    end
+end
+function MOI.set!(mock::MockSolverInstance, ::MockConstraintAttribute, idx::Vector{<:MOI.ConstraintIndex}, value)
+    for (i,v) in zip(idx, value)
+        mock.conattribute[i] = v
+    end
+end
+
+
+MOI.canget(mock::MockSolverInstance, ::MOI.ResultCount) = mock.solved
+MOI.canget(mock::MockSolverInstance, ::MOI.TerminationStatus) = mock.solved
+MOI.canget(mock::MockSolverInstance, ::MOI.ObjectiveValue) = mock.solved # TODO: may want to simulate false
+MOI.canget(mock::MockSolverInstance, ::MOI.PrimalStatus) = mock.solved && (mock.resultcount > 0)
+MOI.canget(mock::MockSolverInstance, ::MockInstanceAttribute) = true
+
+# We assume that a full result is loaded if resultcount > 0
+MOI.canget(mock::MockSolverInstance, ::MOI.VariablePrimal, ::MOI.VariableIndex) = (mock.resultcount > 0)
+MOI.canget(mock::MockSolverInstance, ::MOI.VariablePrimal, ::Vector{MOI.VariableIndex}) = (mock.resultcount > 0)
+
+MOI.canget(mock::MockSolverInstance, ::MockVariableAttribute, idx::MOI.VariableIndex) = haskey(mock.varattribute, idx)
+MOI.canget(mock::MockSolverInstance, ::MockVariableAttribute, idx::Vector{MOI.VariableIndex}) = all(haskey.(mock.varattribute, idx))
+MOI.canget(mock::MockSolverInstance, ::MockConstraintAttribute, idx::MOI.ConstraintIndex) = haskey(mock.conattribute, idx)
+MOI.canget(mock::MockSolverInstance, ::MockConstraintAttribute, idx::Vector{<:MOI.ConstraintIndex}) = all(haskey.(mock.conattribute, idx))
+
+MOI.get(mock::MockSolverInstance, ::MOI.ResultCount) = mock.resultcount
+MOI.get(mock::MockSolverInstance, ::MOI.TerminationStatus) = mock.terminationstatus
+MOI.get(mock::MockSolverInstance, ::MOI.ObjectiveValue) = mock.objectivevalue
+MOI.get(mock::MockSolverInstance, ::MOI.PrimalStatus) = mock.primalstatus
+MOI.get(mock::MockSolverInstance, ::MOI.DualStatus) = mock.dualstatus
+MOI.get(mock::MockSolverInstance, ::MockInstanceAttribute) = mock.attribute
+
+MOI.get(mock::MockSolverInstance, ::MockVariableAttribute, idx::MOI.VariableIndex) = mock.varattribute[idx]
+MOI.get(mock::MockSolverInstance, ::MockVariableAttribute, idx::Vector{MOI.VariableIndex}) = getindex.(mock.varattribute, idx)
+MOI.get(mock::MockSolverInstance, ::MOI.VariablePrimal, idx::MOI.VariableIndex) = mock.varprimal[idx]
+MOI.get(mock::MockSolverInstance, ::MOI.VariablePrimal, idx::Vector{MOI.VariableIndex}) = getindex.(mock.varprimal, idx)
+MOI.get(mock::MockSolverInstance, ::MockConstraintAttribute, idx::MOI.ConstraintIndex) = mock.conattribute[idx]
+MOI.get(mock::MockSolverInstance, ::MockConstraintAttribute, idx::Vector{<:MOI.ConstraintIndex}) = getindex.(mock.conattribute, idx)
---
+++ test/mocksolverinstance.jl
@@ -0,0 +1,81 @@
+
+@MOIU.instance InstanceForMock (ZeroOne, Integer) (EqualTo, GreaterThan, LessThan, Interval) (Zeros, Nonnegatives, Nonpositives, SecondOrderCone, RotatedSecondOrderCone, PositiveSemidefiniteConeTriangle) () (SingleVariable,) (ScalarAffineFunction,ScalarQuadraticFunction) (VectorOfVariables,) (VectorAffineFunction,)
+
+
+@testset "Mock solver instance attributes" begin
+    instance = MOIU.MockSolverInstance(InstanceForMock{Float64}())
+    @test MOI.canset(instance, MOIU.MockInstanceAttribute())
+    MOI.set!(instance, MOIU.MockInstanceAttribute(), 10)
+    @test MOI.canget(instance, MOIU.MockInstanceAttribute())
+    @test MOI.get(instance, MOIU.MockInstanceAttribute()) == 10
+
+    v1 = MOI.addvariable!(instance)
+    @test MOI.canset(instance, MOIU.MockVariableAttribute(), v1)
+    MOI.set!(instance, MOIU.MockVariableAttribute(), v1, 11)
+    @test MOI.canget(instance, MOIU.MockVariableAttribute(), v1)
+    @test MOI.get(instance, MOIU.MockVariableAttribute(), v1) == 11
+    @test MOI.canset(instance, MOIU.MockVariableAttribute(), [v1])
+    MOI.set!(instance, MOIU.MockVariableAttribute(), [v1], [-11])
+    @test MOI.canget(instance, MOIU.MockVariableAttribute(), [v1])
+    @test MOI.get(instance, MOIU.MockVariableAttribute(), [v1]) == [-11]
+
+    c1 = MOI.addconstraint!(instance, MOI.SingleVariable(v1), MOI.GreaterThan(1.0))
+    @test MOI.canset(instance, MOIU.MockConstraintAttribute(), c1)
+    MOI.set!(instance, MOIU.MockConstraintAttribute(), c1, 12)
+    @test MOI.canget(instance, MOIU.MockConstraintAttribute(), c1)
+    @test MOI.get(instance, MOIU.MockConstraintAttribute(), c1) == 12
+    @test MOI.canset(instance, MOIU.MockConstraintAttribute(), [c1])
+    MOI.set!(instance, MOIU.MockConstraintAttribute(), [c1], [-12])
+    @test MOI.canget(instance, MOIU.MockConstraintAttribute(), [c1])
+    @test MOI.get(instance, MOIU.MockConstraintAttribute(), [c1]) == [-12]
+end
+
+@testset "Mock solver instance solve no result" begin
+    instance = MOIU.MockSolverInstance(InstanceForMock{Float64}())
+
+    v1 = MOI.addvariable!(instance)
+
+    # Load fake solution
+    MOI.set!(instance, MOI.TerminationStatus(), MOI.InfeasibleNoResult)
+
+    # Attributes are hidden until after optimize!()
+    @test !MOI.canget(instance, MOI.TerminationStatus())
+    @test !MOI.canget(instance, MOI.ResultCount())
+    @test !MOI.canget(instance, MOI.VariablePrimal())
+
+    MOI.optimize!(instance)
+    @test MOI.canget(instance, MOI.TerminationStatus())
+    @test MOI.canget(instance, MOI.ResultCount())
+    @test !MOI.canget(instance, MOI.VariablePrimal(), v1)
+    @test MOI.get(instance, MOI.TerminationStatus()) == MOI.InfeasibleNoResult
+    @test MOI.get(instance, MOI.ResultCount()) == 0
+end
+
+@testset "Mock solver instance solve with result" begin
+    instance = MOIU.MockSolverInstance(InstanceForMock{Float64}())
+
+    v = MOI.addvariables!(instance, 2)
+
+    # Load fake solution
+    # TODO: Provide a more compact API for this.
+    MOI.set!(instance, MOI.TerminationStatus(), MOI.Success)
+    MOI.set!(instance, MOI.ObjectiveValue(), 1.0)
+    MOI.set!(instance, MOI.ResultCount(), 1)
+    MOI.set!(instance, MOI.VariablePrimal(), v, [1.0, 2.0])
+    MOI.set!(instance, MOI.VariablePrimal(), v[1], 3.0)
+
+    # Attributes are hidden until after optimize!()
+    @test !MOI.canget(instance, MOI.TerminationStatus())
+    @test !MOI.canget(instance, MOI.ResultCount())
+    @test !MOI.canget(instance, MOI.VariablePrimal())
+
+    MOI.optimize!(instance)
+    @test MOI.canget(instance, MOI.TerminationStatus())
+    @test MOI.canget(instance, MOI.ResultCount())
+    @test MOI.canget(instance, MOI.VariablePrimal(), v)
+    @test MOI.canget(instance, MOI.VariablePrimal(), v[1])
+    @test MOI.get(instance, MOI.TerminationStatus()) == MOI.Success
+    @test MOI.get(instance, MOI.ResultCount()) == 1
+    @test MOI.get(instance, MOI.VariablePrimal(), v) == [3.0, 2.0]
+    @test MOI.get(instance, MOI.VariablePrimal(), v[1]) == 3.0
+end
