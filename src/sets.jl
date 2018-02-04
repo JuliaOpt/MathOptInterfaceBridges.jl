c68fc0bc90770fb05e709a465d43c4f49e58d640
Benoît Legat <benoit.legat@gmail.com>
2018-02-04 14:49:04 +0100

    Remove deepcopy as it is automatically implemented

--- src/sets.jl
+++ src/sets.jl
@@ -7,9 +7,6 @@ getconstant(s::MOI.EqualTo) = s.value
 getconstant(s::MOI.GreaterThan) = s.lower
 getconstant(s::MOI.LessThan) = s.upper
 
-Base.deepcopy(s::MOI.AbstractSet) = s
-Base.deepcopy{S<:Union{MOI.SOS1, MOI.SOS2}}(s::S) = S(copy(s.weights))
-
 const DimensionUpdatableSets = Union{MOI.Reals,
                                      MOI.Zeros,
                                      MOI.Nonnegatives,
